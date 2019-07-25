87f0e5a59ac7dfdadef127b7b92eaf7ff51e4425
kschroader <kschroader@hotmail.com>
2019-07-25 17:01:00 -0400

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-$(document).ready(function () {
-    $('#click_to_slide').live('click', function () {
-        $('#slide_me_down').slideDown();
-    });
-});
 
-
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
